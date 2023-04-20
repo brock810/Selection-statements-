c31c257bf5bb62505e5c46e56d5e6c1997afe51c
brock810 <130688727+brock810@users.noreply.github.com>
2023-04-20 14:52:59 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Net.Http.Headers;
+
+namespace Selection_statements_2
+{
+    class program
+    {
+       public static void Main(string[] args)
+        {
+            var userGuess = int.Parse(Console.ReadLine());
+            var favouriteNumber = 694;
+
+            if (userGuess < favouriteNumber)
+            {
+                Console.WriteLine($"TOO low");
+            }
+            else if (userGuess > favouriteNumber)
+            {
+                Console.WriteLine($"TOO high");
+            }
+            if (userGuess == favouriteNumber)
+            {
+                Console.WriteLine($"You got it!");
+
+                Console.WriteLine("What is your favorite subject?");
+
+                string subject = Console.ReadLine();
+
+                switch (subject)
+                {
+                    case "Math":
+                        Console.WriteLine("I hate math");
+                        break;
+                    case "Science":
+                        Console.WriteLine("I love science");
+                        break;
+                    case "Gym":
+                        Console.WriteLine("Gym is awesome");
+                        break;
+                    case "History":
+                        Console.WriteLine("History is radical");
+                        break;
+                    case "Geo":
+                        Console.WriteLine("Geo sucks");
+                        break;
+                    default:
+                        Console.WriteLine("I don't know that subject");
+                        break;
+                }
+
+
+
+            }
+
+
+            }
+
+        }
+
+    }
+
